0d3b71a72a6b93c9db33a44908fefbff1dfdd298
akshay <akshaypotdar99@gmail.com>
2025-10-03 21:30:54 +0530

    changes from askhay

--- OrangeHRMTests/Application/Add_Employee/test_Add_Employee.py
+++ OrangeHRMTests/Application/Add_Employee/test_Add_Employee.py
@@ -1,3 +1,5 @@
+import time
+
 from BaseSeleniumFramework.Utils import config
 from OrangeHRMPOM.Application.Add_Employee.AddEmployee import AddEmployee
 from OrangeHRMPOM.Application.Login.LoginPage import LoginPage
@@ -8,11 +10,13 @@ import pytest
 class Test_Add_Employee:
     def test_add_employee(self,driver):
         emp = AddEmployee(driver)
-        login = LoginPage(driver)
-        login.goto()
-        login.fill_credentials(config.USERNAME,config.PASSWORD)
-        login.click_login()
         emp.navigate_to_pim_menu()
+        emp.navigate_to_add_employee_menu()
+        emp.add_name(middlename="asp456",lastname="asp789")
+        emp.upload_profile_picture(r"C:\MY SPACE\TEST GENERIC ASSETS\JLR Assets 1\Test_asp.jpeg")
+        emp.toggle_switch_login_details()
+        emp.enter_login_details(username="asp123",password="asp456",confirm_password="asp456")
+        time.sleep(5)
 
 
 
